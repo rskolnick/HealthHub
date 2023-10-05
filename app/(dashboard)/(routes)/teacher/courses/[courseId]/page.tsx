382b9795a6620bfe9c942464232b528e30a9daf8
Ryan Skolnick <rhskolnick@gmail.com>
2023-10-04 22:42:59 -0400

    Finished title edit and description edit

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,8 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -49,6 +51,11 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 						<IconBadge icon={LayoutDashboard} />
 						<h2 className='text-xl'>Customize Your Course</h2>
 					</div>
+					<TitleForm initialData={course} courseId={course.id} />
+					<DescriptionForm
+						initialData={course}
+						courseId={course.id}
+					/>
 				</div>
 			</div>
 		</div>
