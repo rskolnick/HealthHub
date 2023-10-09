850a9874097651c331dcf939e91d5b9166e79c51
Ryan Skolnick <rhskolnick@gmail.com>
2023-10-08 20:59:28 -0400

    Finished course attachment form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,19 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import {
+	CircleDollarSign,
+	File,
+	LayoutDashboard,
+	ListChecks,
+} from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -19,6 +25,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const course = await db.course.findUnique({
 		where: {
 			id: params.courseId,
+		},
+		include: {
+			attachments: {
+				orderBy: {
+					createdAt: "desc",
+				},
+			},
 		},
 	});
 
@@ -88,7 +101,16 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 							<IconBadge icon={CircleDollarSign} />
 							<h2 className='text-xl pl-2'>Sell Your Course</h2>
 						</div>
-						<PriceForm
+						<PriceForm initialData={course} courseId={course.id} />
+					</div>
+					<div className='border-t-slate-300 border-t-2 pt-4'>
+						<div className='flex items-center gap-x-2'>
+							<IconBadge icon={File} />
+							<h2 className='text-xl pl-2'>
+								Resources & Attachments
+							</h2>
+						</div>
+						<AttachmentForm
 							initialData={course}
 							courseId={course.id}
 						/>
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -14,7 +14,7 @@ const handleAuth = () => {
 
 export const ourFileRouter = {
   courseImage: f({image: {maxFileSize: "4MB", maxFileCount: 1}}).middleware(() => handleAuth()).onUploadComplete(() => {}),
-  courseAttachement: f(['text', 'image', 'video', 'audio', 'pdf']).middleware(() => handleAuth()).onUploadComplete(() => {}),
+  courseAttachment: f(['text', 'image', 'video', 'audio', 'pdf']).middleware(() => handleAuth()).onUploadComplete(() => {}),
   chapterVideo: f({video: {maxFileCount: 1, maxFileSize: '512GB'}}).middleware(() => handleAuth()).onUploadComplete(() => {})
 } satisfies FileRouter;
  
