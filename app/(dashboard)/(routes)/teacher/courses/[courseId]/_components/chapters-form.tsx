b455549567e784b95a00f8426dab0f9985276e99
Ryan Skolnick <rhskolnick@gmail.com>
2023-10-12 16:14:03 -0400

    Added loader state to chapter dragger

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -13,7 +13,7 @@ import {
 	FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -81,7 +81,12 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
 	};
 
 	return (
-		<div className='mt-6 border bg-slate-100 rounded-md p-4'>
+		<div className='relative mt-6 border bg-slate-100 rounded-md p-4'>
+			{isUpdating && (
+				<div className='absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center'>
+					<Loader2 className='animate-spin h-6 w-6 text-sky-700' />
+				</div>
+			)}
 			<div className='font-medium flex items-center justify-between'>
 				Course Chapters
 				<Button variant='ghost' onClick={toggleCreating}>
