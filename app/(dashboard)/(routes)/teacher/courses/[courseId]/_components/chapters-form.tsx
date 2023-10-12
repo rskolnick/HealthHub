1be54e3251a1e82882171e228d786dc2cfc8d394
Ryan Skolnick <rhskolnick@gmail.com>
2023-10-12 17:00:46 -0400

    Finished chapter edit title

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -80,6 +80,10 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
 		}
 	};
 
+	const onEdit = (id: string) => {
+		router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+	};
+
 	return (
 		<div className='relative mt-6 border bg-slate-100 rounded-md p-4'>
 			{isUpdating && (
@@ -142,7 +146,7 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
 				>
 					{!initialData.chapters.length && "No Chapters"}
 					<ChaptersList
-						onEdit={() => {}}
+						onEdit={onEdit}
 						onReorder={onReorder}
 						items={initialData.chapters || []}
 					/>
