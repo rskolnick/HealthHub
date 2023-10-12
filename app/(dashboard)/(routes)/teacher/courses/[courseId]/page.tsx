f2bfc9ce46519f6a3e12d9b677d1f6a9b7e77049
Ryan Skolnick <rhskolnick@gmail.com>
2023-10-12 15:04:30 -0400

    began course chapter section

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -14,6 +14,7 @@ import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
+import { ChaptersForm } from "./_components/chapters-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -25,8 +26,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const course = await db.course.findUnique({
 		where: {
 			id: params.courseId,
+			userId,
 		},
 		include: {
+			chapters: {
+				orderBy: {
+					position: "asc",
+				},
+			},
 			attachments: {
 				orderBy: {
 					createdAt: "desc",
@@ -51,6 +58,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 		course.imageUrl,
 		course.price,
 		course.categoryId,
+		course.chapters.some((chapter) => chapter.isPublished),
 	];
 
 	const totalFields = requiredFields.length;
@@ -94,7 +102,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 							<IconBadge icon={ListChecks} />
 							<h2 className='text-xl pl-2'>Course Chapters</h2>
 						</div>
-						<div>TODO: Chapters</div>
+						<ChaptersForm
+							initialData={course}
+							courseId={course.id}
+						/>
 					</div>
 					<div className='border-t-slate-300 border-t-2 pt-4'>
 						<div className='flex items-center gap-x-2'>
